860b8a4adac19ef190ee36bd53aef3c38d65ed57
Berend Sliedrecht <beri@void.void>
2022-04-04 18:55:11 +0200

    added linux specific files
    Signed-off-by: Berend Sliedrecht <beri@void.void>

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -1,3 +1,5 @@
+#include <X11/XF86keysym.h>
+
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
@@ -61,10 +63,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *brightupcmd[] = {"xbacklight", "-inc", "10", NULL};
+static const char *brightdowncmd[] = {"xbacklight", "-dec", "10", NULL};
+static const char *volupcmd[] = {"amixer", "-q", "sset", "Master" ,"5%+", NULL};
+static const char *voldowncmd[] = {"amixer", "-q", "sset", "Master" ,"5%-", NULL};
+static const char *volmutetogglecmd[] = {"amixer", "-q", "sset", "Master", "toggle", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_space,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -97,6 +104,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+        { 0,                            XF86XK_MonBrightnessUp,    spawn,            {.v = brightupcmd } },
+        { 0,                            XF86XK_MonBrightnessDown,  spawn,            {.v = brightdowncmd } },
+        { 0,                            XF86XK_AudioRaiseVolume,   spawn,            {.v = volupcmd } },
+        { 0,                            XF86XK_AudioLowerVolume,   spawn,            {.v = voldowncmd } },
+        { 0,                            XF86XK_AudioMute,          spawn,            {.v = volmutetogglecmd } },
 };
 
 /* button definitions */
