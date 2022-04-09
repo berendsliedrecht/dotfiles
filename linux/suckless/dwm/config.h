af267a1d8b8ad3dbdc0578942c43713a44dd7ac1
blu3beri <berend@animo.id>
2022-04-09 15:41:32 +0200

    lock script
    Signed-off-by: blu3beri <berend@animo.id>

--- linux/suckless/dwm/config.h
+++ linux/suckless/dwm/config.h
@@ -43,8 +43,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[T]",      tile },    /* first entry is default */
+	{ "[N]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -69,48 +69,50 @@ static const char *volupcmd[] = {"amixer", "-q", "sset", "Master" ,"5%+", NULL};
 static const char *voldowncmd[] = {"amixer", "-q", "sset", "Master" ,"5%-", NULL};
 static const char *volmutetogglecmd[] = {"amixer", "-q", "sset", "Master", "toggle", NULL};
 static const char *mutemictogglecmd[] = {"amixer", "set", "Capture", "toggle", NULL};
+static const char *slockcmd[] = {"slock", NULL};
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,	                XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_p,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-        { 0,                            XF86XK_MonBrightnessUp,    spawn,            {.v = brightupcmd } },
-        { 0,                            XF86XK_MonBrightnessDown,  spawn,            {.v = brightdowncmd } },
-        { 0,                            XF86XK_AudioRaiseVolume,   spawn,            {.v = volupcmd } },
-        { 0,                            XF86XK_AudioLowerVolume,   spawn,            {.v = voldowncmd } },
-        { 0,                            XF86XK_AudioMute,          spawn,            {.v = volmutetogglecmd } },
-        { 0,                            XF86XK_AudioMicMute,       spawn,            {.v = mutemictogglecmd} },
+	/* modifier                     key                        function          argument */
+	{ MODKEY,                       XK_space,                  spawn,            {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return,                 spawn,            {.v = termcmd } },
+	{ MODKEY,                       XK_b,                      togglebar,        {0} },
+	{ MODKEY,                       XK_j,                      focusstack,       {.i = +1 } },
+	{ MODKEY,                       XK_k,                      focusstack,       {.i = -1 } },
+	{ MODKEY,                       XK_i,                      incnmaster,       {.i = +1 } },
+	{ MODKEY,                       XK_d,                      incnmaster,       {.i = -1 } },
+	{ MODKEY,                       XK_h,                      setmfact,         {.f = -0.05} },
+	{ MODKEY,                       XK_l,                      setmfact,         {.f = +0.05} },
+	{ MODKEY,                       XK_Return,                 zoom,             {0} },
+	{ MODKEY,                       XK_Tab,                    view,             {0} },
+	{ MODKEY,	                      XK_q,                      killclient,       {0} },
+	{ MODKEY,                       XK_t,                      setlayout,        {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,                      setlayout,        {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,                      setlayout,        {.v = &layouts[2]} },
+	{ MODKEY,                       XK_p,                      setlayout,        {0} },
+	{ MODKEY|ShiftMask,             XK_space,                  togglefloating,   {0} },
+	{ MODKEY,                       XK_0,                      view,             {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,                      tag,              {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,                  focusmon,         {.i = -1 } },
+	{ MODKEY,                       XK_period,                 focusmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,                  tagmon,           {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,                 tagmon,           {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_q,                      quit,             {0} },
+	{ MODKEY|ShiftMask,             XK_x,                      spawn,            {.v = slockcmd } },
+  { 0,                            XF86XK_MonBrightnessUp,    spawn,            {.v = brightupcmd } },
+  { 0,                            XF86XK_MonBrightnessDown,  spawn,            {.v = brightdowncmd } },
+  { 0,                            XF86XK_AudioRaiseVolume,   spawn,            {.v = volupcmd } },
+  { 0,                            XF86XK_AudioLowerVolume,   spawn,            {.v = voldowncmd } },
+  { 0,                            XF86XK_AudioMute,          spawn,            {.v = volmutetogglecmd } },
+  { 0,                            XF86XK_AudioMicMute,       spawn,            {.v = mutemictogglecmd} },
+	TAGKEYS( XK_1, 0)
+	TAGKEYS( XK_2, 1)
+	TAGKEYS( XK_3, 2)
+	TAGKEYS( XK_4, 3)
+	TAGKEYS( XK_5, 4)
+	TAGKEYS( XK_6, 5)
+	TAGKEYS( XK_7, 6)
+	TAGKEYS( XK_8, 7)
+	TAGKEYS( XK_9, 8)
 };
 
 /* button definitions */
