1e36d769ebbc575e220e6f9d02aef26e2b30e887
blu3beri <berend@animo.id>
2022-04-10 15:33:41 +0200

    removed st
    Signed-off-by: blu3beri <berend@animo.id>

--- linux/suckless/dwm/config.h
+++ linux/suckless/dwm/config.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *brightupcmd[] = {"xbacklight", "-inc", "10", NULL};
 static const char *brightdowncmd[] = {"xbacklight", "-dec", "10", NULL};
 static const char *volupcmd[] = {"amixer", "-q", "sset", "Master" ,"5%+", NULL};
