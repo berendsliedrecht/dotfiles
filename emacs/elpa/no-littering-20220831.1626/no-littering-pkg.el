ff6c546838dfaac9707a19b4932845a89a337d93
blu3beri <berend@animo.id>
2022-09-01 18:24:20 +0200

    removed eln and elpa
    Signed-off-by: blu3beri <berend@animo.id>

--- emacs/elpa/no-littering-20220831.1626/no-littering-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from no-littering.el  -*- no-byte-compile: t -*-
-(define-package "no-littering" "20220831.1626" "Help keeping ~/.emacs.d clean" '((emacs "25.1") (compat "28.1.1.0")) :commit "714ae852580e5e416f5e661ff0cfaec780e53d38" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :keywords '("convenience") :url "https://github.com/emacscollective/no-littering")
