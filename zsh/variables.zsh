dc1521ad0c88fcd87409958de3cad300068c4893
blu3beri <berend@animo.id>
2022-12-03 18:36:11 +0100

    added alacritty
    Signed-off-by: blu3beri <berend@animo.id>

--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -1,5 +1,4 @@
 export EDITOR="nvim"
 export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"
 export JAVA_HOME="/opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk/Contents/Home"
-export NODE_BINARY="$(which node)"
 export NOTEBOOK="$HOME/Notebook"
