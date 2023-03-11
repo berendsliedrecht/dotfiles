108290d1e3c2c170d568cf99310b2a16ba75903e
blu3beri <blu3beri@proton.me>
2023-03-11 19:06:13 +0100

    added sketchy bar
    Signed-off-by: blu3beri <blu3beri@proton.me>

---
+++ sketchybar/plugins/sleep.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# Script to make macOS sleep
+
+osascript -e 'tell application "System Events" to sleep'
---
+++ sketchybar/plugins/volume.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+VOLUME=$INFO
+
+sketchybar --set $NAME label="[V: $VOLUME%]"
